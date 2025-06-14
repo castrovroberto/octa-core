246d5a278ce6307b4fc3450051405a3be71941e2
Roberto Castro Valenzuela <rob1fcv21@gmail.com>
2025-06-14 06:08:51 -0600

    Refactor GameEngine to accept map and logic

--- src/main/java/tech/yump/engine/GameEngine.java
+++ src/main/java/tech/yump/engine/GameEngine.java
@@ -1,15 +1,32 @@
 package tech.yump.engine;
 
+import tech.yump.core.GameLogic;
 import tech.yump.core.GameMap;
-import tech.yump.model.GridType;
 
+/**
+ * Simple game engine scaffold.  The engine now accepts a {@link GameMap}
+ * and a {@link GameLogic} so tests or alternative game modes can provide
+ * custom implementations.
+ */
 public class GameEngine {
 
-    public void startGame(int size) {
+    private GameMap gameMap;
+    private GameLogic gameLogic;
+
+    /**
+     * Starts the game using the supplied map and logic.
+     *
+     * @param gameMap   the map to use for the game
+     * @param gameLogic the game logic implementation
+     */
+    public void startGame(GameMap gameMap, GameLogic gameLogic) {
+        this.gameMap = gameMap;
+        this.gameLogic = gameLogic;
+
         System.out.println("Game started!");
-        // For now, we hardcode OCTAGONAL as per the requirements.
-        GameMap gameMap = new GameMap(size, GridType.OCTAGONAL);
-        gameMap.printMap();
+        if (this.gameMap != null) {
+            this.gameMap.printMap();
+        }
     }
 
     public void processTurn() {
@@ -20,4 +37,12 @@ public class GameEngine {
         System.out.println("Game ended!");
     }
 
+    // Getter methods for testing purposes
+    public GameMap getGameMap() {
+        return gameMap;
+    }
+
+    public GameLogic getGameLogic() {
+        return gameLogic;
+    }
 }
